1a905e065387cb24142ee3648933f0cabda38c45
AscaL <emanueleorfanelli@gmail.com>
2016-01-22 00:13:47 +0100

    github call working

---
+++ index.js
@@ -0,0 +1,51 @@
+'use strict';
+
+let Rx = require('rx');
+let rp = require('request-promise');
+
+const requestUrl = 'https://api.github.com/users/AscaL';
+const options = {
+	uri: requestUrl,
+	headers: {
+		'User-Agent': 'AscaL'
+	},
+	json: true // Automatically parses the JSON string in the response
+};
+
+let requestStream = Rx.Observable.just(requestUrl);
+
+requestStream.subscribe(function (requestUrl) {
+	let responseStream = Rx.Observable.create(function (observer) {
+		rp(options)
+			.then(resp => {
+				console.log('resp: ', resp);
+			})
+			.catch(error => {
+				console.log('error: ', error);
+			})
+			.always(() => {
+				observer.onComplete();
+			})
+			//			.done(response => {
+			//				observer.onNext(response)
+			//			})
+			//			.fail((jqXHR, status, error) => {
+			//				observer.onError(error)
+			//			})
+	});
+
+	responseStream.subscribe(response => {
+		console.log('response:', response);
+	})
+
+});
+
+
+
+//rp(options)
+//    .then(function (repos) {
+//        console.log(`User has ${repos} repos`);
+//    })
+//    .catch(function (err) {
+//        console.log('err:', err);
+//    });

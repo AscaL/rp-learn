561ac3a51ef83af90eb722ea43502777015c90dd
AscaL <emanueleorfanelli@gmail.com>
2016-01-21 19:55:43 +0100

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+	entry: [
+    'babel-polyfill',
+    './index'
+  ],
+	output: {
+		publicPath: '/',
+		filename: './es5-index.js'
+	},
+	//	devtool: 'source-map',
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				include: path.join(__dirname, 'es6'),
+				loader: 'babel-loader',
+				query: {
+					presets: ["es2015", "stage-0"],
+				}
+      }
+    ]
+	},
+	debug: true
+};

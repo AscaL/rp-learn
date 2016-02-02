8e53bcdce17462382216389d3b33c30e510e159a
AscaL <emanueleorfanelli@gmail.com>
2016-02-02 22:06:40 +0100

    update code

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 let Rx = require('rx');
 let rp = require('request-promise');
 
-const requestUrl = 'https://api.github.com/users';
+const requestUrl = 'https://api.github.com/users/AscaL';
 const options = {
 	uri: requestUrl,
 	headers: {
@@ -18,27 +18,32 @@ requestStream.subscribe(function (requestUrl) {
 	let responseStream = Rx.Observable.create(function (observer) {
 		rp(options)
 			.then(resp => {
-				observer.onNext(resp)
-				observer.onCompleted()
+				observer.onNext(resp);
+				observer.onCompleted();
 			})
 			.catch(error => {
 				console.log('error: ', error);
-				observer.onError(error)
+				observer.onError(error);
 			})
 	});
 
 	responseStream.subscribe(response => {
 		console.log('response:', response);
 	})
-
 });
 
+let responseStream = requestStream.flatMap((options) => {
+	return Rx.Observable.fromPromise(rp(options))
+});
 
+responseStream.subscribe(response => {
+	console.log('response:', response);
+});
 
-//rp(options)
-//    .then(function (repos) {
-//        console.log(`User has ${repos} repos`);
-//    })
-//    .catch(function (err) {
-//        console.log('err:', err);
-//    });
+let refreshButton = document.querySelector('.refresh');
+let refreshClickStream = Rx.Observable.fromEvent(refreshButton, 'click');
+
+let requestStream = refreshClickStream.map(() => {
+	let randomOffset = Math.floor(Math.random() * 500);
+	return 'https://api.github.com/users?since=' + randomOffset;
+});
